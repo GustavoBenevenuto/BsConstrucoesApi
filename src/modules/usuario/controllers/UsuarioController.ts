198bd50b17d8d05ea6fa64d97e989c54169e8f6b
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2023-10-04 15:12:51 -0300

    Criando Controller para o Usuario

---
+++ src/modules/usuario/controllers/UsuarioController.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from 'express';
+import { factoryCriarUsuarioService } from '../factories/factoryCriarUsuarioService';
+import { IUsuarioSemSenha } from '../interfaces/IUsuarioSemSenha';
+import { z } from 'zod';
+import { factoryEditarUsuarioService } from '../factories/factoryEditarUsuarioService';
+import { factoryBuscarUsuarioService } from '../factories/factoryBuscarUsuarioService';
+import { factoryDeletarUsuarioService } from '../factories/factoryDeletarUsuarioService';
+
+export default class UsuarioController {
+    public async criar(request: Request, response: Response): Promise<Response> {
+        const usuarioCorpoSchema = z.object({
+            nome: z.string().min(3),
+            email: z.string().email(),
+            senha: z.string().min(6),
+        })
+
+        const usuario = usuarioCorpoSchema.parse(request.body)
+
+        const criarUsuarioService = factoryCriarUsuarioService()
+
+        const usuarioRetornado: IUsuarioSemSenha = await criarUsuarioService.execute(usuario)
+        delete usuarioRetornado.hash_senha
+
+        return response.json(usuarioRetornado);
+    }
+
+    public async editar(request: Request, response: Response): Promise<Response> {
+        const usuarioCorpoSchema = z.object({
+            nome: z.string().min(3),
+        })
+
+        const usuarioParamsSchema = z.object({
+            id: z.string(),
+        })
+
+        const { nome } = usuarioCorpoSchema.parse(request.body)
+        const { id } = usuarioParamsSchema.parse(request.params)
+
+        const editarUsuarioService = factoryEditarUsuarioService()
+
+        const usuarioRetornado: IUsuarioSemSenha = await editarUsuarioService.execute({ nome, id })
+        delete usuarioRetornado.hash_senha
+
+        return response.json(usuarioRetornado);
+    }
+
+    public async buscar(request: Request, response: Response): Promise<Response> {
+        const usuarioParamsSchema = z.object({
+            id: z.string(),
+        })
+
+        const { id } = usuarioParamsSchema.parse(request.params)
+
+        const buscarUsuarioService = factoryBuscarUsuarioService()
+
+        const usuarioRetornado: IUsuarioSemSenha = await buscarUsuarioService.execute(id)
+        delete usuarioRetornado.hash_senha
+
+        return response.json(usuarioRetornado);
+    }
+
+    public async excluir(request: Request, response: Response): Promise<Response> {
+        const usuarioParamsSchema = z.object({
+            id: z.string(),
+        })
+
+        const { id } = usuarioParamsSchema.parse(request.params)
+
+        const deletarUsuarioService = factoryDeletarUsuarioService()
+        const deletado = await deletarUsuarioService.execute(id)
+
+        return response.json({ deletado });
+    }
+}
--- src/modules/usuario/routes/usuarioRouter.routes.ts
+++ src/modules/usuario/routes/usuarioRouter.routes.ts
@@ -1,76 +1,16 @@
 import { Router } from 'express';
-import { z } from "zod";
-import { factoryCriarUsuarioService } from '../factories/factoryCriarUsuarioService';
-import { factoryEditarUsuarioService } from '../factories/factoryEditarUsuarioService';
-import { factoryBuscarUsuarioService } from '../factories/factoryBuscarUsuarioService';
-import { IUsuarioSemSenha } from '../interfaces/IUsuarioSemSenha';
-import { factoryDeletarUsuarioService } from '../factories/factoryDeletarUsuarioService';
+import UsuarioController from '../controllers/UsuarioController';
 
 const usuarioRouter = Router();
 
-usuarioRouter.post('/', async (request, response) => {
-    const usuarioCorpoSchema = z.object({
-        nome: z.string().min(3),
-        email: z.string().email(),
-        senha: z.string().min(6),
-    })
+const usuarioController = new UsuarioController();
 
-    const usuario = usuarioCorpoSchema.parse(request.body)
+usuarioRouter.post('/', usuarioController.criar);
 
-    const criarUsuarioService = factoryCriarUsuarioService()
+usuarioRouter.patch('/:id', usuarioController.editar);
 
-    const usuarioRetornado: IUsuarioSemSenha = await criarUsuarioService.execute(usuario)
-    delete usuarioRetornado.hash_senha
+usuarioRouter.delete('/:id', usuarioController.excluir);
 
-    return response.json(usuarioRetornado);
-});
-
-usuarioRouter.patch('/:id', async (request, response) => {
-    const usuarioCorpoSchema = z.object({
-        nome: z.string().min(3),
-    })
-
-    const usuarioParamsSchema = z.object({
-        id: z.string(),
-    })
-
-    const { nome } = usuarioCorpoSchema.parse(request.body)
-    const { id } = usuarioParamsSchema.parse(request.params)
-
-    const editarUsuarioService = factoryEditarUsuarioService()
-
-    const usuarioRetornado: IUsuarioSemSenha = await editarUsuarioService.execute({ nome, id })
-    delete usuarioRetornado.hash_senha
-
-    return response.json(usuarioRetornado);
-});
-
-usuarioRouter.delete('/:id', async (request, response) => {
-    const usuarioParamsSchema = z.object({
-        id: z.string(),
-    })
-
-    const { id } = usuarioParamsSchema.parse(request.params)
-
-    const deletarUsuarioService = factoryDeletarUsuarioService()
-    const deletado = await deletarUsuarioService.execute(id)
-
-    return response.json({ deletado });
-});
-
-usuarioRouter.get('/:id', async (request, response) => {
-    const usuarioParamsSchema = z.object({
-        id: z.string(),
-    })
-
-    const { id } = usuarioParamsSchema.parse(request.params)
-
-    const buscarUsuarioService = factoryBuscarUsuarioService()
-
-    const usuarioRetornado: IUsuarioSemSenha = await buscarUsuarioService.execute(id)
-    delete usuarioRetornado.hash_senha
-
-    return response.json(usuarioRetornado);
-});
+usuarioRouter.get('/:id', usuarioController.buscar);
 
 export default usuarioRouter;
