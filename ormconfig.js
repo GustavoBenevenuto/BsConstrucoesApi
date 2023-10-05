38143eddc58b0eaffb643fd94c6f8f8f3e5491c2
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2023-10-05 13:41:16 -0300

    Ajustando variavel de ambiente e configuração do orm

--- ormconfig.js
+++ ormconfig.js
@@ -1,21 +1,21 @@
-
 const orm = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "ssl": { "rejectUnauthorized": false },
     "entities": [
-        `./${process.env.FOLDER_ENTITIES_ORM}/*/models/*.${process.env.FILE_EXTENSION}`,
+        `./${process.env.ROOT_DIR}/modules/*/models/*.${process.env.FILE_EXTENSION}`,
     ],
     "migrations": [
-        `./${process.env.FOLDER_MIGRATIONS_ORM}/*.${process.env.FILE_EXTENSION}`
+        `./${process.env.ROOT_DIR}/database/migrations/*.${process.env.FILE_EXTENSION}`
     ],
     "cli": {
         "migrationsDir": "./src/database/migrations"
     }
 }
 
-if (process.env.ENVIRONMENT == 'desenvolvimento')
+if (process.env.ENVIRONMENT == 'desenvolvimento') {
     delete orm.ssl;
+}
 
 module.exports = orm;
 //"url" : "postgres://username:password@host:port/database"
